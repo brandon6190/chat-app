045746728892d4429a9902148d7e2049ea4bf6c8
brandon6190 <brandon6190@outlook.com>
2019-08-15 16:17:43 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css'; 
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        Hello World
+      </div>
+    )
+  }
+}
+
+export default App;
