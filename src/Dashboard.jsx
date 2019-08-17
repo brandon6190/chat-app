ebc180923b0cba7beec04c77807e6bc90fe171b8
brandon6190 <brandon6190@outlook.com>
2019-08-17 11:56:43 -0400

    Functional Component basic structer for Dashboard. Importing Paper component for background and Typography component

---
+++ src/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+function Dashboard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Dashboard;
