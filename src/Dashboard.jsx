f577897c3dd558a621465651f71f3243b03c1601
brandon6190 <brandon6190@outlook.com>
2019-08-18 12:12:58 -0400

    Added more classes to the makeStyles callback for compoenents, and added apropriete style for layout. Added two div elements for the topicsWindow and chatWindow components

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,13 +1,62 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: '50px',
+    padding: theme.spacing(3, 2),
+  },
+  flex: {
+    display: 'flex'
+  },
+  topicsWindow: {
+    width: '30%',
+    height: '300px',
+    borderRight: '1px solid grey'
+  },
+  chatWindow: {
+    width: '70%',
+    height: '300px'
+  },
+  chatBox: {
+    width: '85%'
+  },
+  button: {
+    width: '15%'
+  }
+}));
 
 function Dashboard() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const classes = useStyles();
+
+	return (
+		<div>
+			<Paper className={classes.root}>
+				<Typography variant="h4" component="h4">
+					Chat App
+				</Typography>
+				<Typography variant="h5" component="h5">
+					Topic placeholder
+				</Typography>
+
+        <div className={classes.flex}>
+          <div className={classes.topicsWindow}>
+
+          </div>
+          <div className={classes.chatWindow}>
+
+          </div>
+        </div>
+
+        <div className={classes.flex}>
+          
+        </div>
+
+			</Paper>
+		</div>
+	);
 }
 
 export default Dashboard;
