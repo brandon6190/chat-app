83478821e1596c2fb20f6574b76e66979d6caf9a
brandon6190 <brandon6190@outlook.com>
2019-09-12 14:22:46 -0400

    -Importing and initializing socket with the http server -Listening to socket connections:   -Log that the user is connected   -Uppons sockets disconnection log that the user disconnected   -When the scoket receives a chat messages log the message to console

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 const port = process.env.PORT || 3001;
+
+io.on('connection', socket => {
+  console.log('user connected');
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ', msg);
+  })
+});
 
 http.listen(port, () => {
   console.log(`Server listening on port ${port}`);
